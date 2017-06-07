4ee6d978f7b1d8556ae3b63f3c2b1ce2cb938d32
ArnedeR <arneder@gmail.com>
2017-06-07 14:30:12 +0200

    db update

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if (env === 'production') {
+	console.log(DATABASE_URL);
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres'
 	})
